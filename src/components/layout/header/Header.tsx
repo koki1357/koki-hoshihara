93678bb9ecf5f19561371eb0985ae716acf0c47d
kokihoshihara <star.kouki.123@gmail.com>
2024-12-09 01:05:44 +0900

    デザインの修正

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,7 +4,6 @@ import './Header.css';
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <h1>自己紹介ページ</h1>
       <nav>
         <ul>
           <li><a href="#profile">プロフィール</a></li>
